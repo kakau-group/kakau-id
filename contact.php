f690ebf74e8064f1758b670fb1e630f3ca8f491f
fadjar setiawan <fadjarsetiawans@gmail.com>
2021-03-12 16:31:49 +0700

    commit awal

---
+++ contact.php
@@ -0,0 +1,46 @@
+<!-- ***** Footer Start ***** -->
+<footer id="contact-us">
+    <div class="container">
+        <div class="footer-content">
+            <div class="row">
+                <!-- ***** Contact Form Start ***** -->
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <div class="form">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.501138706389!2d108.52008591431506!3d-6.708526167453035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6ee14f3e1117f3%3A0xa4258a10676f5a8a!2sKakau%20Laundry!5e0!3m2!1sid!2sid!4v1612935840302!5m2!1sid!2sid" width="1000" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                            </iframe>
+                            </iframe>
+                        </div>
+                        <br><br>
+                            </div>
+                                </div>
+                                
+                            <!-- ***** Contact Form End ***** -->
+                            
+                            <div class="right-content col-lg-6 col-md-12 col-sm-12">
+                                <p>Alamat:
+                                    <br>Jl. Raya Tengah Tani No.59, Dawuan, Kec. Tengah Tani, Kab. Cirebon, Jawa Barat, Indonesia
+                                    <br><br>
+                                <p>Contact:
+                                    <br>+62 823-2131-7255
+                                    <br><br>
+                                <p>E-mail:
+                                    <br>admin@kakau.id
+                                <ul class="social">
+                                    <li><a href="https://www.facebook.com/KakauLaundry"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="https://www.instagram.com/kakau_laundry"><i class="fa fa-instagram"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="sub-footer">
+                    <p>Copyright &copy; 2021 Kakau
+
+                    | fdrs </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ jenis-usaha.php
@@ -0,0 +1,97 @@
+    <!-- ***** Features Big Item Start ***** -->
+    <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                    data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <img src="assets/images/logo-laundry.png" alt="">
+                            <h4>Kakau Laundry</h4>
+                            <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
+                            <br>
+                            <a href="#testimonials" class="btn btn-primary btn-lg">
+                                Kunjungi Situs
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                    data-scroll-reveal="enter bottom move 30px over 0.6s after 0.4s">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <img src="assets/images/logo-baby-shop.png" alt="">
+                            <h4>Kakau Baby Shop</h4>
+                            <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
+                            <br>
+                            <a href="#testimonials" class="btn btn-primary btn-lg">
+                                Kunjungi Situs
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                    data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <img src="assets/images/logo-ac.png" alt="">
+                            <h4>Kakau AC</h4>
+                            <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
+                            <br>
+                            <a href="#testimonials" class="btn btn-primary btn-lg">
+                            Kunjungi Situs
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                    data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <img src="assets/images/logo-gaming.png" alt="">
+                            <h4>Kakau Gaming</h4>
+                            <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
+                            <br>
+                            <a href="#testimonials" class="btn btn-primary btn-lg">
+                                Kunjungi Situs
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                    data-scroll-reveal="enter bottom move 30px over 0.6s after 0.4s">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <img src="assets/images/features-icon-2.png" alt="">
+                            <h4>Kakau Permak</h4>
+                            <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
+                            <br>
+                            <a href="#testimonials" class="btn btn-primary btn-lg">
+                                Kunjungi Situs
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                    data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <img src="assets/images/logo-snack.png" alt="">
+                            <h4>Kakau Snack</h4>
+                            <p>Curabitur pulvinar vel odio sed sagittis. Nam maximus ex diam, nec consectetur diam.</p>
+                            <br>
+                            <a href="#testimonials" class="btn btn-primary btn-lg">
+                            Kunjungi Situs
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Big Item End ***** -->
